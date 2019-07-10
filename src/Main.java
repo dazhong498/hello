37109b9be05cb0ba53dfa3f3c6d811facfdef530
david <david.zhong498@gmail.com>
2019-07-10 12:28:20 -0500

    pushed from laptop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello First Intellj Hello World");
+
+        System.out.println("Pushed from laptop");
     }
 }
