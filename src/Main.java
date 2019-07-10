3a7e9244c4b2e2d5dc1e5b49738cc1aea56d37f3
dazhong498 <david.zhong498@gmail.com>
2019-07-10 12:31:34 -0500

    Changed from desktop
    commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello First Intellj Hello World");
 
-        System.out.println("Pushed from laptop");
+        System.out.println("Changed from desktop");
     }
 }
